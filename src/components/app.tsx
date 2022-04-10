bc6b48d5c66ca463e598b7e9e8226336a75ee8e4
Kevin Brown <kevinmmbrown@outlook.com>
2022-04-09 20:22:10 -0700

    Now loading and saving to local state

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,11 +1,38 @@
 import * as React from 'react';
+import { store } from '../dataLayer/store';
 import { List } from './list';
 import { Menu } from './menu';
+import throttle from 'lodash.throttle';
+import { saveStateToLocalStorage } from '../dataLayer/persistentStorage';
 
 export function App() {
+  const [isSaved, setIsSaved] = React.useState<boolean>(true);
+  const saveTimer = React.useRef<ReturnType<typeof setTimeout> | undefined>(undefined);
+
+  function saveNow() {
+    saveStateToLocalStorage(store.getState());
+    setIsSaved(true);
+  }
+
+  React.useEffect(() => {
+    const unsubscribe = store.subscribe(() => {
+      if (saveTimer.current !== undefined) {
+        clearTimeout(saveTimer.current);
+      }
+      setIsSaved(false);
+      saveTimer.current = setTimeout(() => {
+        saveStateToLocalStorage(store.getState());
+        setIsSaved(true);
+      }, 1000);
+    });
+    return () => {
+      unsubscribe();
+    };
+  }, [setIsSaved]);
+
   return (
     <div>
-      <Menu />
+      <Menu isSaved={isSaved} />
       <List />
     </div>
   );
--- src/dataLayer/store.ts
+++ src/dataLayer/store.ts
@@ -1,15 +1,20 @@
 import { configureStore } from '@reduxjs/toolkit';
-import entriesReducer from './entriesSlice';
-
-const LOCAL_STORAGE_KEY = 'DataModel';
+import entriesReducer, { IEntry } from './entriesSlice';
+import { loadStateFromLocalStorage } from './persistentStorage';
 
 export const store = configureStore({
   reducer: {
     entries: entriesReducer
-  }
+  },
+  preloadedState: loadStateFromLocalStorage()
 });
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
-export type RootState = ReturnType<typeof store.getState>;
+// export type RootState = ReturnType<typeof store.getState>;
+export type RootState = {
+  entries?: {
+    entries: IEntry[];
+  };
+};
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch;
