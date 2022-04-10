211725968cb62ae8d02de8e0a4b710f594d5dd0f
Kevin Brown <kevinmmbrown@outlook.com>
2022-04-09 19:14:08 -0700

    Migrated to createRoot

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 
 import { Provider } from 'react-redux';
 import { store } from './dataLayer/store';
@@ -8,10 +9,10 @@ import { App } from './app';
 
 console.log('index.tsx');
 
-const rootElement = document.getElementById('root');
-ReactDOM.render(
+const container = document.getElementById('root');
+const reactRoot = createRoot(container);
+reactRoot.render(
   <Provider store={store}>
     <App />
-  </Provider>,
-  rootElement
+  </Provider>
 );
