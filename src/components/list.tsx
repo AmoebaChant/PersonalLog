bf1647ebb56ce35e22b5d1c6012325f288ca5a44
Kevin Brown <kevinmmbrown@outlook.com>
2022-04-09 19:36:31 -0700

    Now simple add/remove works with small UI components

--- src/components/list.tsx
+++ src/components/list.tsx
@@ -1,12 +1,11 @@
 import * as React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { IEntry, selectEntries } from '../dataLayer/entriesSlice';
+import { ListItem } from './listItem';
 
 export function List() {
   const entries = useSelector(selectEntries);
-  const dispatch = useDispatch();
-
-  const contents = entries.map((entry: IEntry) => <div key={entry.id}>{entry.name}</div>);
+  const contents = entries.map((entry: IEntry) => <ListItem key={entry.id} entry={entry}></ListItem>);
 
   return <div>{contents}</div>;
 }
