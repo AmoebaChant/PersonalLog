b632a5a8f59a24035032fb63f0f7a201bfff2583
Kevin Brown <kevinmmbrown@outlook.com>
2022-04-09 20:22:10 -0700

    Now loading and saving to local state

--- src/components/menu.tsx
+++ src/components/menu.tsx
@@ -3,14 +3,20 @@ import { useSelector, useDispatch } from 'react-redux';
 import { addEntry, IEntry, removeEntry, selectEntries } from '../dataLayer/entriesSlice';
 import { v4 as uuidv4 } from 'uuid';
 
-export function Menu() {
+export interface IMenuProps {
+  isSaved: boolean;
+}
+
+export function Menu(props: IMenuProps) {
   const entries = useSelector(selectEntries);
   const dispatch = useDispatch();
 
   const contents = entries.map((entry: IEntry) => <div key={entry.id}>{entry.name}</div>);
 
+  const savedState = props.isSaved ? <div>Saved</div> : <div>Saving...</div>;
   return (
     <div className="menuRoot">
+      {savedState}
       <button onClick={() => dispatch(addEntry({ id: uuidv4(), name: Date.now().toString() }))}>Add</button>
     </div>
   );
