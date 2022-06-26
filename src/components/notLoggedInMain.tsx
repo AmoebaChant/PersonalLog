1e1cacc360595510486358be5c4d9a076df490d4
Kevin Brown <kevinmmbrown@outlook.com>
2022-06-26 16:28:35 -0700

    Styled login page

--- src/components/notLoggedInMain.tsx
+++ src/components/notLoggedInMain.tsx
@@ -10,10 +10,21 @@ export function NotLoggedInMain(props: INotLoggedInMain) {
   const loginPhase = useObservable(props.auth.loginPhase);
 
   return (
-    <div>
-      <div className="appTitle">Personal Log</div>
-      <div className="loginState">{loginPhase}</div>
-      {loginPhase === 'loginNeeded' ? <button onClick={() => props.auth.login()}>Log in</button> : <></>}
+    <div className="notLoggedInRoot">
+      <div className="notLoggedInPanel">
+        <div className="appTitle">Personal Log</div>
+        <div className="appSubtitle">
+          A multi-platform logging app that supports hashtags, and stores all of the data in your own OneDrive account
+          in human-readable form so you'll always be able to access it.
+        </div>
+        {loginPhase === 'loginNeeded' ? (
+          <button className="loginButton" onClick={() => props.auth.login()}>
+            Log in
+          </button>
+        ) : (
+          <></>
+        )}
+      </div>
     </div>
   );
 }
