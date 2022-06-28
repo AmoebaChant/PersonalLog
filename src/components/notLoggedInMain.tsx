3cb101c7ec19768675e556f910ca8f5d7866823d
Kevin Brown <kevinmmbrown@outlook.com>
2022-06-27 21:35:18 -0700

    Light mode

--- src/components/notLoggedInMain.tsx
+++ src/components/notLoggedInMain.tsx
@@ -16,6 +16,10 @@ export function NotLoggedInMain(props: INotLoggedInMain) {
         <div className="appSubtitle">
           A multi-platform logging app that supports hashtags, and stores all of the data in your own OneDrive account
           in human-readable form so you'll always be able to access it.
+          <p>
+            This software is under development and breaking changes could be made at any time, causing all previous data
+            to be lost. Use at your own risk, and know that there are no guarantees made about this software.
+          </p>
         </div>
         {loginPhase === 'loginNeeded' ? (
           <button className="loginButton" onClick={() => props.auth.login()}>
