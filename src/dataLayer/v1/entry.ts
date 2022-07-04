276e31ee604c58c1ed26475ca416729992d2968b
Kevin Brown <kevinmmbrown@outlook.com>
2022-07-03 20:38:38 -0700

    starting to add tag field

--- src/dataLayer/v1/entry.ts
+++ src/dataLayer/v1/entry.ts
@@ -3,12 +3,13 @@ import { ITag, IV1Entry } from './schema';
 
 export class V1Entry implements IV1Entry {
   public readonly tags: IObservable<ITag[]> = new Observable<ITag[]>([]);
+  public readonly allTags: IObservable<ITag[]> = new Observable<ITag[]>([]);
   public readonly date: IObservable<string> = new Observable<string>('');
   public readonly body: IObservable<string> = new Observable<string>('');
   public readonly unsubscribers: (() => void)[] = [];
 
-  constructor(public readonly id: string, tags: ITag[], date: string, body: string) {
-    this.tags.value = tags;
+  constructor(public readonly id: string, tags: ITag[], allTags: ITag[], date: string, body: string) {
+    this.allTags.value = allTags;
     this.date.value = date;
     this.body.value = body;
   }
